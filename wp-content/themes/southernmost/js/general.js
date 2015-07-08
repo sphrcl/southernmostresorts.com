ee733c1b620a5d7eb5faeddf4a5c9a5afafca5df
Mandino <dino@misfit-inc.com>
2015-07-08 14:41:56 +0800

    fix IE slidedown nav

--- wp-content/themes/southernmost/js/general.js
+++ wp-content/themes/southernmost/js/general.js
@@ -1,3 +1,30 @@
+function detectIE() {
+    var ua = window.navigator.userAgent;
+
+    var msie = ua.indexOf('MSIE ');
+    if (msie > 0) {
+        // IE 10 or older => return version number
+        return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+    }
+
+    var trident = ua.indexOf('Trident/');
+    if (trident > 0) {
+        // IE 11 => return version number
+        var rv = ua.indexOf('rv:');
+        return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+    }
+
+    var edge = ua.indexOf('Edge/');
+    if (edge > 0) {
+       // IE 12 => return version number
+       return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+    }
+
+    // other browser
+    return false;
+}
+
+var IEversion = detectIE();
 
 var $document = $(document),
     $element = $('body'),
@@ -32,11 +59,18 @@ $(document).ready(function(){
 	
 	$("ul.topnavigation li").hover(function() {
 		$(this).addClass("ahoy");
-		
-		}, function() {
-		
+		if(IEversion <= 9) {
+			$('#header').height(480);
+		}		
+	}, function() {		
 		$(this).removeClass("ahoy");
+		if(IEversion <= 9) {
+			$('#header').height('');
+		}
 	});
+
+
+
 	$(".closer ").click(function(e){
 		e.preventDefault();
 		$("#wrapper").removeClass("opened");
