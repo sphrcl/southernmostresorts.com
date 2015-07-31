020bb1c1813209c3675acbb712bfd958523a4b6b
sphrcl <sphericalcommunications@gmail.com>
2015-07-31 16:50:38 -0400

    adding save the children to footer

--- wp-content/themes/southernmost/footer.php
+++ wp-content/themes/southernmost/footer.php
@@ -71,6 +71,23 @@
 			</ul>
 		</div>
 
+		<!-- save the children 
+
+		<div class="footcolumn">
+
+			<div class="save-the-children">
+				
+					<img class="left" width="270px" src="http://www.southernmostbeachresort.com/wp-content/themes/southernmost/images/save-the-children-logo.png">
+					
+					<p class="save">Join us in supporting Save the Children, internationally recognized for giving children a healthy start, the opportunity to learn and protection from harm.100% of your donation benefits Save the Children. Thank you for your support.</p>
+
+					<a class="button" target="_blank" href="https://secure.savethechildren.org/site/c.8rKLIXMGIpI4E/b.6239401/k.C01C/Global_Action_Fund/apps/ka/sd/donor.asp?msource=cpkhhgaf1214&utm_source=Highgate2014&utm_medium=link&utm_campaign=highgatehotels1214">donate now</a>
+
+			</div>
+
+		</div>
+
+		<!-- / save the children -->
 
 	</div>
 
