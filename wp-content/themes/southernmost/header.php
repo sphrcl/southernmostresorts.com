9222c69188aa55ccaebf219b2499e071dd72670c
Jhelline <jhelline.fausto@outsourced.ph>
2016-03-28 18:57:48 +0800

    update GA Tag

--- wp-content/themes/southernmost/header.php
+++ wp-content/themes/southernmost/header.php
@@ -57,19 +57,17 @@
 		wp_head();
 	?>
 
-	<script>
+	<script> 
 
 		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-		ga('create', 'UA-17032284-1', 'auto', {
-			'allowLinker': true
-		});
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga'); 
+		ga('create', 'UA-17032284-1', 'auto',{
+			'allowLinker': true 
+		}); ga('require', 'linker'); 
+		ga('linker:autoLink', ['splitrockresort.reztrip.com','splitrockresort.reztripmobile.com']); 
 		ga('send', 'pageview');
-		ga('require', 'linker');
-		ga('linker:autoLink', ['reztrip.com'], false, true);
 
 	</script>
 
