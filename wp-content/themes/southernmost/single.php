34b335c8be571fca58fa3c312abb55ac19e58168
jomel <jomel_bola@yahoo.com>
2016-05-24 21:05:07 +0800

    neighborhood

--- wp-content/themes/southernmost/single.php
+++ wp-content/themes/southernmost/single.php
@@ -14,6 +14,95 @@ get_header(); ?>
 	$imgsrc = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), "Full"); 
 ?>
 
+<?php if($post->ID==2479) { ?>
+
+
+<div id="topbanner">
+
+	<div class="static-banner" style="background-image: url('<?php echo tt($imgsrc[0],1400,755); ?>')"></div>
+
+	<div class="topbanner-overlay"></div>
+
+
+<?php endwhile; endif; wp_reset_query(); ?>	
+
+
+</div>
+	
+<div class="innerpage wrapper specials_page">
+	<div id="pagecontent">
+
+		<div class="container">
+
+			<div class="contenttitle">
+				
+				<?php 
+					if(have_posts()) : while(have_posts()) : the_post(); 
+					$imgsrc = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), "Full"); 
+				?>
+
+				<h1 class="title"><?php the_title(); ?></h1>
+				<h2 class="subtitle"><?php echo get_post_meta($post->ID,'misfit_shortname',true); ?></h2>
+				
+				<?php endwhile; endif; wp_reset_query(); ?>	
+
+			</div>
+
+			<div class="socialmedia">
+				<ul>
+					<?php if(get_option('misfit_facebook')) { ?><li><a href="<?php echo get_option('misfit_facebook'); ?>" target="_blank"><i class="fa fa-facebook"></i></a></li><?php } ?>
+					<?php if(get_option('misfit_twitter')) { ?><li><a href="<?php echo get_option('misfit_twitter'); ?>" target="_blank"><i class="fa fa-twitter"></i></a></li><?php } ?>
+					<?php if(get_option('misfit_youtube')) { ?><li><a href="<?php echo get_option('misfit_youtube'); ?>" target="_blank"><i class="fa fa-youtube-play"></i></a></li><?php } ?>
+					<?php if(get_option('misfit_instagram')) { ?><li><a href="<?php echo get_option('misfit_instagram'); ?>" target="_blank"><i class="fa fa-instagram"></i></a></li><?php } ?>
+					<?php if(get_option('misfit_google_plus')) { ?><li><a href="<?php echo get_option('misfit_google_plus'); ?>" target="_blank"><i class="fa fa-google-plus"></i></a></li><?php } ?>
+					<?php if(get_option('misfit_tripadvisor')) { ?><li><a href="<?php echo get_option('misfit_tripadvisor'); ?>" target="_blank"><i class="fa fa-tripadvisor"></i></a></li><?php } ?>
+				</ul>
+			</div>
+
+			<div class="content">
+				<!-- the_content(); -->
+				<?php the_content(); ?>	
+			</div>
+
+		</div>
+
+	</div>
+
+	<div id="specials_list">
+	<?php 
+
+			$dining = new WP_Query(array(
+				'post_type' => 'neighborhood',
+				'posts_per_page' => -1
+			));
+
+			if($dining->have_posts()) : while($dining->have_posts()) : $dining->the_post(); 
+			$imgsrc = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), "Full");
+
+		?>
+		<div class="specialsbox" style="background-image: url(<?php echo tt($imgsrc[0],1400,755); ?>);">
+			<div class="specialscontent">
+				<div class="specialtitle">
+					<h3><?php the_title(); ?></h3>
+					<h4><?php echo excerpt(20); ?></h4>
+				</div>
+				<div class="specialbuttons">
+					<a class="button7" href="<?php the_permalink(); ?>">Details</a>
+				</div>
+			</div>
+		</div>
+
+		<?php endwhile; endif; wp_reset_query(); ?>	
+		
+		
+		
+	</div>
+</div>
+
+
+<?php
+}else{ ?>
+
 	<div id="topbanner">
 
 		<div class="static-banner" style="background-image: url('<?php echo tt($imgsrc[0],1400,755); ?>')"></div>
@@ -91,6 +180,10 @@ get_header(); ?>
 	</div>
 
 <?php endwhile; endif; wp_reset_query(); ?>	
+
+<?php } ?>
+
+
 
 	</div> <!-- #wrapper -->
 
