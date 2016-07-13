b7e2583f08ae253ff9176e77de12cb2ccd8600f0
Gil <gil.concepcion@outsourced.ph>
2016-07-13 17:01:22 +0800

    Special List Re Order

--- wp-content/themes/southernmost/page-activitiesnew.php
+++ wp-content/themes/southernmost/page-activitiesnew.php
@@ -94,7 +94,19 @@
 
 		<div id="specials_list">
 
-
+ <?php $content3=get_page(2486); 
+	  $imgsrc = wp_get_attachment_image_src( get_post_thumbnail_id( $content3->ID ), "Full");
+  ?>	<a href="<?php echo get_the_permalink($content3->ID); ?>">
+		<div class="specialsbox" style="background-image: url(<?php echo tt($imgsrc[0],1400,755); ?>);">
+			
+			<div class="specialscontent">
+				<div class="specialtitle">
+					<h3><?php echo $content3->post_title; ?> </h3>
+					 
+				</div>
+				 
+			</div>
+		</div></a>		
 
 <?php $content1=get_page(445); 
 	  $imgsrc = wp_get_attachment_image_src( get_post_thumbnail_id( $content1->ID ), "Full");
@@ -125,23 +137,7 @@
 				 
 			</div>
 		</div></a>
- 		
- <?php $content3=get_page(2486); 
-	  $imgsrc = wp_get_attachment_image_src( get_post_thumbnail_id( $content3->ID ), "Full");
-  ?>	<a href="<?php echo get_the_permalink($content3->ID); ?>">
-		<div class="specialsbox" style="background-image: url(<?php echo tt($imgsrc[0],1400,755); ?>);">
-			
-			<div class="specialscontent">
-				<div class="specialtitle">
-					<h3><?php echo $content3->post_title; ?> </h3>
-					 
-				</div>
-				 
-			</div>
-		</div></a>			
- 
-
-  		
+	   		
 <?php $content4=get_page(1285); 
 	  $imgsrc = wp_get_attachment_image_src( get_post_thumbnail_id( $content4->ID ), "Full");
   ?>	<a href="<?php echo get_the_permalink($content4->ID); ?>">
