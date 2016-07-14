e3002bb2beceb177765471a407aadf31929ce8b9
jomel <jomel_bola@yahoo.com>
2016-07-14 18:33:46 +0800

    Revert Menu

--- wp-content/themes/southernmost/footer.php
+++ wp-content/themes/southernmost/footer.php
@@ -171,63 +171,7 @@
 					
 					<div class="srnclose"><i class="fa fa-close"></i></div>
 
-					
-
-					<ul class="activitiespress">
-						
-						 <?php $content1=get_page(445); 
-	                        $imgsrc = wp_get_attachment_image_src( get_post_thumbnail_id( $content1->ID ), "Medium");
-                          ?>	
-						
-						<li>
-							<a href="<?php echo get_the_permalink($content1->ID); ?>">
-								<div style="background-image: url(<?php echo tt($imgsrc[0],320,240); ?>);"></div>
-								<h3><?php echo $content1->post_title; ?> </h3>
-							</a>
-						</li>
-
-
-
-						 <?php $content2=get_page(2488);
-	                      $imgsrc2 = wp_get_attachment_image_src( get_post_thumbnail_id( $content2->ID ), "Medium");
-                          ?>	
-						
-						<li>
-							<a href="<?php echo get_the_permalink($content2->ID); ?>">
-								<div style="background-image: url(<?php echo tt($imgsrc2[0],320,240); ?>);"></div>
-								<h3><?php echo $content2->post_title; ?> </h3>
-							</a>
-						</li>
-
-
-
-						 <?php $content3=get_page(2486); 
-	  $imgsrc3 = wp_get_attachment_image_src( get_post_thumbnail_id( $content3->ID ), "Medium");
-                          ?>	
-						
-						<li>
-							<a href="<?php echo get_the_permalink($content3->ID); ?>">
-								<div style="background-image: url(<?php echo tt($imgsrc3[0],320,240); ?>);"></div>
-								<h3><?php echo $content3->post_title; ?> </h3>
-							</a>
-						</li>
-
-
-						 <?php   $content4=get_page(1285); 
-	  $imgsrc4 = wp_get_attachment_image_src( get_post_thumbnail_id( $content4->ID ), "Medium");
-                          ?>	
-						
-						<li>
-							<a href="<?php echo get_the_permalink($content4->ID); ?>">
-								<div style="background-image: url(<?php echo tt($imgsrc4[0],320,240); ?>);"></div>
-								<h3><?php echo $content4->post_title; ?> </h3>
-							</a>
-						</li>
-						
-					 
-						
-					</ul>
-					
+	
 					
 					<ul class="pressroom">
 						
