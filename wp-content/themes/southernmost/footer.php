1dac979f24984d330e03fd04fac5cc2518e8590e
mandino <dino@misfit-inc.com>
2016-12-06 18:31:17 +0800

    change button text

--- wp-content/themes/southernmost/footer.php
+++ wp-content/themes/southernmost/footer.php
@@ -108,7 +108,7 @@
 				
 				<p class="save">Southernmost Beach Resort Key West is a proud supporter of Save the Children, internationally recognized for giving children a healthy start, the opportunity to learn and protection from harm. To donate and make a difference in a child’s life, please click on the link below. 100% of your donation benefits Save the Children. Thank you for your support.</p>
 
-				<a class="button" target="_blank" href="https://goo.gl/OJexpZ">donate now</a>
+				<a class="button" target="_blank" href="https://goo.gl/OJexpZ">donate today</a>
 		</div>
 
 
